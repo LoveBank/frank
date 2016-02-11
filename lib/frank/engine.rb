bd7d212bad48860ac04b60939df1b9d6c420c00a
Jared L Jennings <jared@jaredjennings.org>
2016-02-11 09:57:37 -0600

    Handle migrations
    Hopefully this will help with applications handling migrations that the
    engine has.
    
    https://blog.pivotal.io/labs/labs/leave-your-migrations-in-your-rails-en
    gines

--- lib/frank/engine.rb
+++ lib/frank/engine.rb
@@ -6,5 +6,13 @@ module Frank
       g.test_framework :rspec, fixture: false
       g.fixture_replacement :factory_girl, dir: 'spec/factories'
     end
+
+    # to handle migrations to implementing apps
+    ## https://blog.pivotal.io/labs/labs/leave-your-migrations-in-your-rails-engines
+    initializer :append_migrations do |app|
+      unless app.root.to_s.match root.to_s
+        app.config.paths["db/migrate"] += config.paths["db/migrate"].expanded
+      end
+    end
   end
 end
